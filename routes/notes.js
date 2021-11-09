7ad06b727a066746dfad8e0229f38c702db113e9
leonaysmael16 <ysmealleona@gmail.com>
2021-11-09 11:16:49 -0800

    added notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,4 +1,26 @@
 const fs = require('fs');
 const util = require('util');
 
-const
+const { v4:uuidv4 } = require('uuid');
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function read() {
+    return readFileAsync('db/ddb.json', 'utf8');
+}
+
+const getNotes = function () {
+    return read().then((notes) => {
+        let parsedNotes = JSON.parse(notes) || [];
+        return parsedNotes
+    });
+}
+
+function write(note) {
+    return writeFileAsync('db/db.json', JSON.stringify(note));
+}
+
+function deleteNote(id) {
+    return getNotes().then((notes) => notes.filter((note) => note.id !== id))
+    .then((filteredNotes) => write(filteredNotes));
+}
