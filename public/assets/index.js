47927f78c944c16161b3bfddb0de60c66d3107dc
leonaysmael16 <ysmealleona@gmail.com>
2021-11-10 22:24:30 -0800

    added code to index.js

--- public/assets/index.js
+++ public/assets/index.js
@@ -22,3 +22,64 @@ const getNotes = () =>
             'Content-Type': 'application/json',
         },
     });
+
+const saveNotes = (note) => 
+fetch('/api/notes', {
+    method: 'POST',
+    headers: {
+        'Content': 'application/json',
+    },
+    body: JSON.stringify(note),
+});
+
+const deleteNote = (id) => 
+    fetch(`/api/notes/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+const activeNote = () => {
+    hide(saveNoteBtn);
+
+    if (activeNote.id) {
+        noteTitle.setAttribute('read', true);
+        noteText.setAttribute('read', true);
+        noteTitle.value = activeNote.title;
+        noteTitle.value = activeNote.text;
+    } else {
+        noteTitle.removeAttribute('read');
+        noteText.removeAttribute('read');
+        noteTitle.value = '?';
+        noteText.value = '?';
+    }
+};
+
+const noteSaveHandle = () => {
+    const newNote = {
+        title: noteValue.value,
+        text: noteText.value,
+    };
+    saveNotes(newNote).then(() => {
+        getActiveNotes();
+        activeNote();
+    });
+};
+
+const deleteNoteHandle = (e) => {
+    e.stopPropagation();
+
+    const note = e.target;
+    const noteId = JSON.parse(note.parseElement.getAttribute('data-note')).id;
+
+    if (activeNote.id === noteId) {
+        activeNote = {};
+    }
+
+    deleteNote(noteId).then(() => {
+        getandRenderNotes();
+        activeNote();
+    });
+};
+
