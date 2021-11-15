0ea4b728a3ff0d2b9ef9a8a76d9f33d3bf732402
leonaysmael16 <ysmealleona@gmail.com>
2021-11-14 21:10:06 -0800

    push test

--- server.js
+++ server.js
@@ -4,3 +4,19 @@ const path = require('path');
 const PORT = process.env.PORT || 3306;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+require('./routes/notes');
+
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    );
+
+    app.get('*', (req, res) => 
+    res.sendFile(path.join)
+    )
+
+    // commit test
