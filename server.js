f6ec773bc0b65794520cc196f91a65af22ba25d6
leonaysmael16 <ysmealleona@gmail.com>
2021-12-01 18:39:41 -0800

    added changes

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 
 
-const PORT = process.env.PORT || 3306
+const PORT = process.env.PORT || 3001
 
-const app = express()
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,12 +12,18 @@ app.use(express.static('public'));
 
 require('./routes/notes');
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/note.html'))
+});
+
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
     );
 
-    app.get('*', (req, res) => 
-    res.sendFile(path.join)
-    )
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
-    // commit test
+app.listen(PORT, () => 
+    console.log(`App Listening at http://localhost:${PORT}`)
+);
